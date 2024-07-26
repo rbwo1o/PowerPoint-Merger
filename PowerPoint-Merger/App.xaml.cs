d26c7f097730839610ceb5a1fbdd921c08c0bba9
rbw_o1o <rbw_o1o@DESKTOP-9GC8PTV>
2024-07-26 18:25:43 -0500

    Add project files.

---
+++ PowerPoint-Merger/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace PowerPoint_Merger
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
